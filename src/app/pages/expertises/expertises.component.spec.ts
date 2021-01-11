ef77a00605d183fe2215b47c2627ecc3cf78efbb
fagnigue <fagniguecoulibaly@yahoo.com>
2021-01-11 08:15:24 +0000

    ok

---
+++ src/app/pages/expertises/expertises.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExpertisesComponent } from './expertises.component';
+
+describe('ExpertisesComponent', () => {
+  let component: ExpertisesComponent;
+  let fixture: ComponentFixture<ExpertisesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ExpertisesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExpertisesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
